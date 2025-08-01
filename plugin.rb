9bf6c43a83e00cdffa1f6b40407e6ef6e52334fe
Jeffrey Wu <wu19930628@gmail.com>
2025-08-01 23:12:19 +0800

    feat: disable honeypot method

--- plugin.rb
+++ plugin.rb
@@ -9,12 +9,23 @@ after_initialize do
   module DisableSuspiciousRequestCheck
     def respond_to_suspicious_request
       Rails.logger.info("[DisableSuspiciousCheck] respond_to_suspicious_request called but doing nothing")
-      return
     end
     
     def suspicious?(params)
       Rails.logger.info("[DisableSuspiciousCheck] suspicious? called - always returning false")
-      return false
+      false
+    end
+    
+    def honeypot_or_challenge_fails?(params)
+      Rails.logger.info("[DisableSuspiciousCheck] honeypot_or_challenge_fails? called - always returning false")
+      false
+    end
+    
+    def account_created
+      Rails.logger.info("[DisableSuspiciousCheck] account_created method called")
+      Rails.logger.info("[DisableSuspiciousCheck] Session data: #{session.inspect}")
+      Rails.logger.info("[DisableSuspiciousCheck] Current user: #{current_user.inspect}")
+      super
     end
   end
 
@@ -22,5 +33,5 @@ after_initialize do
   
   UsersController.prepend(DisableSuspiciousRequestCheck)
   
-  Rails.logger.info("[DisableSuspiciousCheck] Plugin loaded successfully - both methods overridden")
+  Rails.logger.info("[DisableSuspiciousCheck] Plugin loaded successfully - multiple methods overridden")
 end
