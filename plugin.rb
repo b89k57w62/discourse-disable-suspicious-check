4079b930b7146d85c999330b29ebe587aa262417
Jeffrey Wu <wu19930628@gmail.com>
2025-08-01 23:29:11 +0800

    update: add debug log

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@
 after_initialize do
   module DisableSuspiciousRequestCheck
     def respond_to_suspicious_request
-      Rails.logger.info("[DisableSuspiciousCheck] respond_to_suspicious_request called but doing nothing")
+      Rails.logger.info("[DisableSuspiciousCheck] respond_to_suspicious_request called - bypassed completely")
     end
     
     def suspicious?(params)
@@ -21,10 +21,31 @@ after_initialize do
       false
     end
     
+    def create
+      Rails.logger.info("[DisableSuspiciousCheck] create method called - tracking session")
+      Rails.logger.info("[DisableSuspiciousCheck] Before create - session keys: #{session.keys.inspect}")
+      
+      result = super
+      
+      Rails.logger.info("[DisableSuspiciousCheck] After create - session keys: #{session.keys.inspect}")
+      Rails.logger.info("[DisableSuspiciousCheck] Response status: #{response.status}")
+      Rails.logger.info("[DisableSuspiciousCheck] Session user_created_message: #{session['user_created_message'].inspect}")
+      
+      result
+    end
+    
     def account_created
-      Rails.logger.info("[DisableSuspiciousCheck] account_created method called")
-      Rails.logger.info("[DisableSuspiciousCheck] Session data: #{session.inspect}")
-      Rails.logger.info("[DisableSuspiciousCheck] Current user: #{current_user.inspect}")
+      Rails.logger.info("[DisableSuspiciousCheck] account_created called")
+      Rails.logger.info("[DisableSuspiciousCheck] Session all keys: #{session.keys.inspect}")
+      Rails.logger.info("[DisableSuspiciousCheck] Session user_created_message: #{session['user_created_message'].inspect}")
+      Rails.logger.info("[DisableSuspiciousCheck] Current user: #{current_user&.username}")
+
+      if session["user_created_message"].blank?
+        Rails.logger.warn("[DisableSuspiciousCheck] Missing user_created_message in session, setting default")
+        session["user_created_message"] = I18n.t("activation.success") || "Account created successfully"
+        Rails.logger.info("[DisableSuspiciousCheck] Session after setting default: #{session['user_created_message']}")
+      end
+      
       super
     end
   end
@@ -33,5 +54,5 @@ after_initialize do
   
   UsersController.prepend(DisableSuspiciousRequestCheck)
   
-  Rails.logger.info("[DisableSuspiciousCheck] Plugin loaded successfully - multiple methods overridden")
+  Rails.logger.info("[DisableSuspiciousCheck] Plugin loaded - suspicious check disabled with session fix")
 end
